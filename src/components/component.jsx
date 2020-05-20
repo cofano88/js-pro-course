d8a6d5efa4de3b169800a488ac93af2d8d36714c
cofano88 <60695878+cofano88@users.noreply.github.com>
2020-05-20 15:30:43 +0300

    20-05-20

---
+++ src/components/component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./component.css";
+
+export class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
+  reload() {
+    this.setState({
+      count: 0,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="clicker">
+          <div className="counter">{this.state.count}</div>
+          <div className="panel">
+            <button className="increment" onClick={() => this.increment()}>
+              +
+            </button>
+            <button className="reload" onClick={() => this.reload()}></button>
+            <button className="decrement" onClick={() => this.decrement()}>
+              -
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Component } from "./components/component";
+
+ReactDOM.render(<Component />, document.getElementById("root"));
