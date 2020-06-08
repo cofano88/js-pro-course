773524055b706808e85b60f1e7711509858c3d51
cofano88 <60695878+cofano88@users.noreply.github.com>
2020-06-08 23:42:10 +0300

    calc

--- src/HW/Calc/App.jsx
+++ src/HW/Calc/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
+import "./App.css";
 
 export function App() {
   const [display, setDisplay] = useState("0");
-  const [arg1, setArg1] = useState("");
-  const [arg2, setArg2] = useState("");
 
   function changeDisplay(arg) {
     if (display === "0") {
@@ -14,34 +13,56 @@ export function App() {
   }
 
   function result() {
-    setDisplay(+display);
+    setDisplay(eval(display));
   }
 
   return (
     <>
-      <div className="calc">
-        <div className="display">{display}</div>
-        <div className="keyboard">
-          <div className="numsClear">
-            <div className="clear" onClick={() => setDisplay("0")}>
-              clear
+      <div className="calcWrap">
+        <div className="calc">
+          <div className="display">{display}</div>
+          <div className="keyboard">
+            <div className="numsClear">
+              <div className="clear" onClick={() => setDisplay("0")}>
+                clear
+              </div>
+              <div className="num" onClick={() => changeDisplay("0")}>
+                0
+              </div>
+              <div className="num" onClick={() => changeDisplay("1")}>
+                1
+              </div>
+              <div className="num" onClick={() => changeDisplay("2")}>
+                2
+              </div>
+              <div className="num" onClick={() => changeDisplay("3")}>
+                3
+              </div>
+              <div className="num" onClick={() => changeDisplay("4")}>
+                4
+              </div>
+              <div className="num" onClick={() => changeDisplay("5")}>
+                5
+              </div>
+              <div className="num" onClick={() => changeDisplay("6")}>
+                6
+              </div>
+              <div className="num" onClick={() => changeDisplay("7")}>
+                7
+              </div>
+              <div className="num" onClick={() => changeDisplay("8")}>
+                8
+              </div>
+              <div className="num" onClick={() => changeDisplay("9")}>
+                9
+              </div>
             </div>
-            <div onClick={() => changeDisplay("0")}>0</div>
-            <div onClick={() => changeDisplay("1")}>1</div>
-            <div onClick={() => changeDisplay("2")}>2</div>
-            <div onClick={() => changeDisplay("3")}>3</div>
-            <div onClick={() => changeDisplay("4")}>4</div>
-            <div onClick={() => changeDisplay("5")}>5</div>
-            <div onClick={() => changeDisplay("6")}>6</div>
-            <div onClick={() => changeDisplay("7")}>7</div>
-            <div onClick={() => changeDisplay("8")}>8</div>
-            <div onClick={() => changeDisplay("9")}>9</div>
-          </div>
-          <div className="tools">
-            <div onClick={() => changeDisplay("+")}>"+"</div>
-            <div>"-"</div>
-            <div>"/"</div>
-            <div>"="</div>
+            <div className="tools">
+              <div onClick={() => changeDisplay("+")}>+</div>
+              <div onClick={() => changeDisplay("-")}>-</div>
+              <div onClick={() => changeDisplay("/")}>/</div>
+              <div onClick={() => result()}>=</div>
+            </div>
           </div>
         </div>
       </div>
